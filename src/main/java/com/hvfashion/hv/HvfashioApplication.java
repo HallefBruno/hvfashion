f559cf12f47e0015bccbbc46c924086dc27f482e
brunohallef <brunohallef@gmail.com>
2024-07-27 10:26:52 -0300

    Enviando para git hub

---
+++ src/main/java/com/hvfashion/hv/HvfashioApplication.java
@@ -0,0 +1,13 @@
+package com.hvfashion.hv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvfashioApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HvfashioApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hvfashion/hv/HvfashioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvfashion.hv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvfashioApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
